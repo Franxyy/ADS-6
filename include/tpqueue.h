778585ece2ec538beffd844d0941841b65b889e4
Franxyy <curmeevvladislav@gmail.com>
2026-05-02 10:50:20 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,35 +5,35 @@
 template<typename T>
 class TPQueue {
  public:
-   TPQueue() : head(nullptr) {}
+ TPQueue() : head(nullptr) {}
 
-   void push(const T& v) {
-     Node* n = new Node(v);
-     if (!head || v.prior > head->val.prior) {
-       n->next = head;
-       head = n;
-     } else {
-       Node* cur = head;
-       while (cur->next && cur->next->val.prior >= v.prior)
-         cur = cur->next;
-         n->next = cur->next;
-         cur->next = n;
-     }
+ void push(const T& v) {
+   Node* n = new Node(v);
+   if (!head || v.prior > head->val.prior) {
+     n->next = head;
+     head = n;
+   } else {
+     Node* cur = head;
+     while (cur->next && cur->next->val.prior >= v.prior)
+       cur = cur->next;
+       n->next = cur->next;
+       cur->next = n;
    }
-   T pop() {
-     T res = head->val;
-     Node* old = head;
-     head = head->next;
-     delete old;
-     return res;
-    }
-  private:
-    struct Node {
-      T val;
-      Node* next;
-      explicit Node(const T& v) : val(v), next(nullptr) {}
-    };
-    Node* head;
+ }
+ T pop() {
+   T res = head->val;
+   Node* old = head;
+   head = head->next;
+   delete old;
+   return res;
+ }
+ private:
+  struct Node {
+    T val;
+    Node* next;
+    explicit Node(const T& v) : val(v), next(nullptr) {}
+  };
+  Node* head;
 };
 
 struct SYM {
